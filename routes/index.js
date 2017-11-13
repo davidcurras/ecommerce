ef998a77b1a6316160e3cf8aedd58aa1521a21b7
Marcelo Dosko <marcelodosko@yahoo.com.ar>
2017-11-13 13:44:21 -0300

    ecommerce#4: General API

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 const user = require('./user')
+const general = require('./general')
 const static = require('./static')
 
-module.exports = [].concat(user, static)
+module.exports = [].concat(user, general, static)
