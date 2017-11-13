a994ac928eccfb6bebf422bcd6382e22735f2e98
David Curras <davidc.ferre@gmail.com>
2017-11-13 17:04:07 -0300

    ecommerce#15: HTTP Errors and Request Validation with JOI and Boom

--- routes/user.js
+++ routes/user.js
@@ -1,4 +1,6 @@
 const _ = require('lodash')
+const joi = require('joi')
+const boom = require('boom')
 const data = require('../data')
 
 const routes = [
@@ -13,31 +15,61 @@ const routes = [
   {
     method: 'GET',
     path: '/user/{id}',
+    config: {
+      validate: {
+        params: joi.object({
+          id: joi.number()
+        })
+      }
+    },
     handler: (request, reply) => {
       const userId = parseInt(request.params.id)
       console.log('GET /user/'+userId)
       const user = _.find(data.users, {'id': userId})
       if (user) reply(user)
-      else reply({error: 'not found'})
+      else reply(boom.notFound('User '+userId+' not found'))
     }
   },
   {
     method: 'GET',
     path: '/user/{key}/{value}',
+    config: {
+      validate: {
+        params: joi.object({
+          key: joi.string(),
+          value: joi.string(),
+        })
+      }
+    },
     handler: (request, reply) => {
       const key = request.params.key
       const value = request.params.value
-      console.log('GET /user/'+key + '/' +value);
+      console.log('GET /user/' + key + '/' + value);
       const filter = {}
       filter[key] = value
       const users = _.filter(data.users, filter)
       if (users && users.length) reply(users)
-      else reply({error: 'not found'})
+      else reply(boom.notFound('No matches'))
     }
   },
   {
     method: 'POST',
     path: '/user',
+    config: {
+      validate: {
+        payload: joi.object({
+          id: joi.number().required(),
+          username: joi.string().required(),
+          pass: joi.string().required(),
+          email: joi.string().required(),
+          name: joi.string().required(),
+          lastName: joi.string().required(),
+          address: joi.string().required(),
+          phone: joi.string().allow(''),
+          isAdmin: joi.boolean()
+        })
+      }
+    },
     handler: (request, reply) => {
       console.log('POST /user/')
       data.users.push(request.payload)
@@ -47,6 +79,24 @@ const routes = [
   {
     method: 'PUT',
     path: '/user/{id}',
+    config: {
+      validate: {
+        params: joi.object({
+          id: joi.number()
+        }),
+        payload: joi.object({
+          id: joi.number().required(),
+          username: joi.string().required(),
+          pass: joi.string().required(),
+          email: joi.string().required(),
+          name: joi.string().required(),
+          lastName: joi.string().required(),
+          address: joi.string().required(),
+          phone: joi.string().allow(''),
+          isAdmin: joi.boolean()
+        })
+      },
+    },
     handler: (request, reply) => {
       const userId = parseInt(request.params.id)
       console.log('PUT /users/'+userId)
@@ -55,12 +105,19 @@ const routes = [
         data.users[index] = request.payload
         reply(data.users[index])
       }
-      else reply({error: 'not found'})
+      else reply(boom.notFound('User '+userId+' not found'))
     }
   },
   {
     method: 'DELETE',
     path: '/user/{id}',
+    config: {
+      validate: {
+        params: joi.object({
+          id: joi.number()
+        })
+      }
+    },
     handler: (request, reply) => {
       const userId = parseInt(request.params.id)
       console.log('DELETE /users/'+userId)
@@ -69,7 +126,7 @@ const routes = [
         data.users.splice(index, 1)
         reply({deleted: true})
       }
-      else reply({error: 'not found'})
+      else reply(boom.notFound('User '+userId+' not found'))
     }
   }
 ]
