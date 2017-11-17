f440b7575a37f33b47a3da821148ed226be9749d
Marcelo Dosko <marcelodosko@yahoo.com.ar>
2017-11-17 14:39:18 -0300

    ecommerce #9: Filter Products by keywords and category filters

--- routes/product.js
+++ routes/product.js
@@ -1,4 +1,7 @@
 const _ = require('lodash')
+const joi = require('joi')
+const boom = require('boom')
+const utils = require('../utils')
 const data = require('../data')
 
 const routes = [
@@ -10,20 +13,36 @@ const routes = [
       reply(data.products)
     }
   },
+
   {
     method: 'GET',
     path: '/product/{id}',
+    config: {
+      validate: {
+        params: joi.object({
+          id: joi.number()
+        })
+      }
+    },
     handler: (request, reply) => {
       const productId = parseInt(request.params.id)
       console.log('GET /product/'+productId)
       const product = _.find(data.products, {'id': productId})
       if (product) reply(product)
-      else reply({error: 'not found'})
+      else reply(boom.notFound('Product '+productId+' not found'))
     }
   },
+
   {
     method: 'GET',
     path: '/product/filter/{filterId}',
+    config: {
+      validate: {
+        params: joi.object({
+          filterId: joi.number()
+        })
+      }
+    },
     handler: (request, reply) => {
       const filterId = parseInt(request.params.filterId)
       console.log('GET /product/filter/'+filterId)
@@ -34,13 +53,21 @@ const routes = [
                           )
                         )
       if (products && products.length) reply(products)
-      else reply({error: 'not found'})
+      else reply(boom.notFound('Not exist Producs for the filterId'+filterId))
     }
   },
 
   {
     method: 'GET',
     path: '/product/category/{categoryId}/filter/{filterId}',
+    config: {
+      validate: {
+        params: joi.object({
+          categoryId: joi.number(),
+          filterId: joi.number()
+        })
+      }
+    },
     handler: (request, reply) => {
       const categoryId = parseInt(request.params.categoryId)
       const filterId = parseInt(request.params.filterId)
@@ -55,44 +82,90 @@ const routes = [
                           )
                         )
       if (productsCatFilter && productsCatFilter.length) reply(productsCatFilter)
-      else reply({error: 'not found'})
+      else reply(boom.notFound(
+        'Not exist Producs for the filterId '+filterId+' and categoryId '+categoryId
+      ))
     }
   },
   {
     method: 'POST',
     path: '/product',
+    config: {
+      validate: {
+        payload: joi.object({
+          id: joi.number().required(),
+          categoryId: joi.number().required(),
+          name: joi.string().required(),
+          description: joi.string().required(),
+          image: joi.string().allow(''),
+          stock: joi.number().required(),
+          price: joi.number().required(),
+          offer: joi.number().allow(null),
+          filters: joi.array().allow([])
+        })
+      }
+    },
     handler: (request, reply) => {
       console.log('POST /product/')
       data.products.push(request.payload)
+      utils.writeFile('../data/products.json', data.products)
       reply(request.payload)
     }
   },
   {
     method: 'PUT',
     path: '/product/{id}',
+    config: {
+      validate: {
+        params: joi.object({
+          id: joi.number()
+        }),
+        payload: joi.object({
+          id: joi.number().required(),
+          categoryId: joi.number().required(),
+          name: joi.string().required(),
+          description: joi.string().required(),
+          image: joi.string().allow(''),
+          stock: joi.number().required(),
+          price: joi.number().required(),
+          offer: joi.number().allow(null),
+          filters: joi.array().allow([])
+        })
+      }
+    },
     handler: (request, reply) => {
       const productId = parseInt(request.params.id)
       console.log('PUT /products/'+productId)
       const index = _.findIndex(data.products, {'id': productId})
       if (index >= 0) {
         data.products[index] = request.payload
+        utils.writeFile('../data/products.json', data.products)
         reply(data.products[index])
       }
-      else reply({error: 'not found'})
+      else reply(boom.notFound('Product '+productId+' not found'))
     }
   },
+
   {
     method: 'DELETE',
     path: '/product/{id}',
+    config: {
+      validate: {
+        params: joi.object({
+          id: joi.number()
+        })
+      }
+    },
     handler: (request, reply) => {
       const productId = parseInt(request.params.id)
       console.log('DELETE /products/'+productId)
       const index = _.findIndex(data.products, {'id': productId})
       if (index >= 0) {
         data.products.splice(index, 1)
+        utils.writeFile('../data/products.json', data.products)
         reply({deleted: true})
       }
-      else reply({error: 'not found'})
+      else reply(boom.notFound('Product '+productId+' not deleted'))
     }
   }
 ]
